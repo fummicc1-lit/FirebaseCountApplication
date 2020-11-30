f8307e8d3b12c0b509c34a9973d2c587f20c9dba
fummicc1 <fumiya.tennis1@gmail.com>
2020-12-01 00:31:29 +0900

    Update for start branch.

--- FirebaseCountApplication/ViewController.swift
+++ FirebaseCountApplication/ViewController.swift
@@ -11,10 +11,8 @@ import FirebaseFirestore
 class ViewController: UIViewController {
     
     // MARK: IBOutlet
-    @IBOutlet weak var label: UILabel!
     
     // MARK: 変数
-    var number: Int = 0
     let ref: DocumentReference = Firestore.firestore().collection("counts").document("checkwork")
     
     // MARK: override
@@ -23,34 +21,5 @@ class ViewController: UIViewController {
     }
     
     // MARK: @IBAction
-    @IBAction func plus() {
-        number += 1
-        updateLabel()
-    }
-    
-    @IBAction func minus() {
-        number -= 1
-        updateLabel()
-    }
-    
-    @IBAction func multiply() {
-        number *= 2
-        updateLabel()
-    }
-    
-    @IBAction func divide() {
-        number /= 2
-        updateLabel()
-    }
-    
-    @IBAction func clear() {
-        number = 0
-        updateLabel()
-    }
-    
-    // MARK: ラベルに数字を表示するメソッド
-    func updateLabel() {
-        label.text = String(number)
-    }
 }
 
