27bbaba688b71d7cba8708c319b56bcedce81579
fummicc1 <fumiya.tennis1@gmail.com>
2020-11-30 23:21:19 +0900

    Initial Commit

---
+++ FirebaseCountApplication/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirebaseCountApplication
+//
+//  Created by Fumiya Tanaka on 2020/11/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
